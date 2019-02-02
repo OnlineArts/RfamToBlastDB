cc23124df538424284a62f3352a2dd42b0e50aab
Roman Martin <roman.martin@uni-marburg.de>
2019-02-02 16:57:51 +0100

    Add live status

--- RfamDownload.sh
+++ RfamDownload.sh
@@ -8,8 +8,12 @@ echo "Rfam version: $version"
 echo "Start downloading"
 
 echo '' > rfam.fa
-for i in {00001..02889}; do
-  echo "$i / 02889"
+to=002889
+t=$(echo $to | sed 's/^0*//')
+for i in `seq -w 000001 $to`; do
+  ii=$(echo $i | sed 's/^0*//')
+  p=`awk "BEGIN{print int(($ii/$t)*100)}"`
+  echo -ne "\r${p} % ($i:$to)"
   wget ftp://ftp.ebi.ac.uk/pub/databases/Rfam/$version/fasta_files/RF$i.fa.gz -O RF$i.fa.gz > /dev/null 2>&1
   if (( "$?" != 0)); then
     continue;
